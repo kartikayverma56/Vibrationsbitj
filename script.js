04b0cec7d92dcce102f0a6da83b7fdae2335f1a1
kartikayverma56 <122208593+kartikayverma56@users.noreply.github.com>
2024-01-30 01:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Events
+var popupViews = document.querySelectorAll('.popup-view');
+var popupBtns = document.querySelectorAll('.popup-btn');
+var closeBtns = document.querySelectorAll('.close-btn');
+
+//javascript for quick view button
+var popup = function (popupClick) {
+  popupViews[popupClick].classList.add('active');
+}
+
+popupBtns.forEach((popupBtn, i) => {
+  popupBtn.addEventListener("click", () => {
+	popup(i);
+  });
+});
+
+//javascript for close button
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", () => {
+	popupViews.forEach((popupView) => {
+	  popupView.classList.remove('active');
+	});
+  });
+});
+
+
+//filter
+document.getElementById("eventSearch").addEventListener("input", function () {
+  var searchTerm = this.value.toLowerCase();
+  var eventNames = document.querySelectorAll('.name');
+
+  eventNames.forEach(function (eventName) {
+	var eventNameText = eventName.innerText.toLowerCase();
+	var card = eventName.closest('.product');
+
+	if (eventNameText.includes(searchTerm)) {
+	  card.style.display = "block";
+	} else {
+	  card.style.display = "none";
+	}
+  });
+});
